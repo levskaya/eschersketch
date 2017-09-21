d130be6b7e6b1ccd15091509c59be826b09f9b6a
Anselm Levskaya <levskaya@gmail.com>
2017-09-21 16:58:34 -0700

    fixed pencil tool to have live styling and to behave like other tools

--- src/pencilTool.js
+++ src/pencilTool.js
@@ -24,7 +24,6 @@ import {simplifyPoints} from './simplifyPoints'; // Douglas-Peucker
 
 // Draw Raw Mousepath (Pencil)
 //------------------------------------------------------------------------------
-//TODO: add smoothing factor
 export class PencilOp {
   constructor(symmState, ctxStyle, points, options) {
     this.tool = "pencil";
@@ -37,7 +36,6 @@ export class PencilOp {
   render(ctx){
     _.assign(ctx, this.ctxStyle);
     updateSymmetry(this.symmState);
-    //gS.$emit('symmUpdate', this.symmState);
     for (let af of affineset) {
       ctx.beginPath();
       const Tpt0 = af.onVec(this.points[0]);
@@ -69,19 +67,25 @@ export class PencilTool {
   constructor() {
     this.points = [];
     this.state = _INIT_;
-    this.liverender = this.liverender_fast;
+    this.liverender = this.liverender_precise;
     this.options = {
         simplify: {val:0.6, type: "slider", min:0.4, max:10, step:0.2},
     };
+    this.actions = [
+      {name: "cancel", desc: "cancel", icon: "icon-cross", key: "Escape"},
+      {name: "commit", desc: "start new (automatic on new click)", icon: "icon-checkmark", key: "Enter"},
+    ];
   }
 
   liverender_precise() {
+    if(this.state===_INIT_){return;} //empty data case
     lctx.clearRect(0, 0, canvas.width, canvas.height);
+    let simplifiedPoints = simplifyPoints(this.points, this.options.simplify.val, true);
     for (let af of affineset) {
       lctx.beginPath();
-      const Tpt0 = af.onVec(this.points[0]);
+      const Tpt0 = af.onVec(simplifiedPoints[0]);
       lctx.moveTo(Tpt0[0], Tpt0[1]);
-      for (let pt of this.points.slice(1)) {
+      for (let pt of simplifiedPoints.slice(1)) {
         const Tpt = af.onVec(pt);
         lctx.lineTo(Tpt[0], Tpt[1]);
       }
@@ -89,6 +93,8 @@ export class PencilTool {
     }
   }
 
+  // fast, hacky live rendering routine that adds points to path as they're added instead of redrawing
+  // the entire path for each added point - quite expensive for long paths with all of the symmetric draw calls!
   liverender_fast() {
     lctx.save();
     // correct alpha to give accurate preview transparency given the 3-pt line overlap that this hack uses:
@@ -101,7 +107,7 @@ export class PencilTool {
         const Tpt1 = af.onVec(this.points[this.points.length-2]);
         const Tpt2 = af.onVec(this.points[this.points.length-1]);
 
-        // A global pressure var from pressure.js actually works... hmm!
+        // A global pressure var from pressure.js actually works.
         //lctx.save();
         //lctx.lineWidth = pressure*30;
         //lctx.strokeStyle = "rgba(200,100,100,"+pressure+")";
@@ -156,16 +162,19 @@ export class PencilTool {
     commitOp(new PencilOp(symmState, ctxStyle, simplifiedPoints, bakeOptions(this.options)));
     //console.log("pencil: point reduction", this.points.length, simplifiedPoints.length);
     lctx.clearRect(0, 0, livecanvas.width, livecanvas.height);
+    this.points = [];
+    this.state = _INIT_;
   }
 
-  cancel() { //not used, just here for consistency
+  cancel() {
+    lctx.clearRect(0, 0, livecanvas.width, livecanvas.height);
     this.points = [];
     this.state = _INIT_;
   }
 
   mouseDown(e) {
     if(this.state==_OFF_) {
-      this.commit(); //XXX: autocommit
+      this.commit(); //XXX: commit on new line
     }
     this.state = _ON_;
     let {left, top} = livecanvas.getBoundingClientRect();
@@ -176,15 +185,22 @@ export class PencilTool {
     if (this.state == _ON_) {
       let {left, top} = livecanvas.getBoundingClientRect();
       this.points.push([e.clientX - left, e.clientY - top]);
-      this.liverender();
+      this.liverender_fast();
     }
   }
 
   mouseUp(e) {
-    if(this.state != _INIT_){
-      this.commit(); //XXX: autocommit
-      this.points = [];
-      this.state = _INIT_;
+    this.state = _OFF_;
+    this.liverender_precise();
+  }
+
+  keyDown(e) {
+    if(e.target.type){return;} // don't interfere with input UI key-events
+    for(let action of this.actions){
+      if(action.key == e.code){
+        this[action.name]();
+      }
     }
   }
+
 }
